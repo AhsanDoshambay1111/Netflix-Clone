bfaac8e3ca10d24dcf29073e125377f47d8453d8
AhsanDoshambay1111 <ajdashti5@gmail.com>
2024-02-09 18:52:36 +0500

    Ahsan Updated the code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import Home from './Pages/Home'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
 import Login from './Pages/Login';
 
 function App() {
   return (
-    <>
     <Router>
-    <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Login" element={<Login />} />
-      </Routes>
-    </div>
-  </Router>
-  </>
-  )
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
